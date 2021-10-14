43bcb5d866a395d873419379c1fc78a8366e6d0f
Damian Tarnawsky <damian@ionic.io>
2021-10-14 06:31:42 -0700

    Add array of arrays

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,6 +11,9 @@ export class HomePage {
 
   constructor(private popoverController: PopoverController) {}
 
+  grouped = [1,2,3,4,5,6,7,8,9,10];
+  grouped2 = [1,2,3,4,5,6,7,8,9,10];
+
   async showPop(ev: any) {
     const popover = await this.popoverController.create({
       component: PopContentComponent,      
